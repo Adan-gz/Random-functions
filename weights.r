76045a8298c7b47b0a4e66c0573d6d2f360b5688
Adan-gz <74647601+Adan-gz@users.noreply.github.com>
2022-01-13 17:10:04 +0000

    Create weights.r

---
+++ weights.r
@@ -0,0 +1,75 @@
+# In this file I share two functions to facilitate weighting your data. First of all, a function to apply simple weighting,
+# i.e. if in the population there 50% males and 50% females but in your sample the values are 60% and 40%, 
+# giving more weight to females and less to males when calculating any summary stats. 
+# After creating one weighting vector for each desired variable, these vectors are multiplied in order to 
+# obtain the unique weighting vector for the data.
+
+###### The function
+
+#' Create a vector of weights
+#' 
+#' Given a population distribution, a vector of weights 
+#' is obtained to equalize said distribution. 
+#'
+#' @param data A dataframe or tibble.
+#' @param population A list with named vectors. Each vector contains the 
+#'    population proportions.
+#' @param return_df TRUE if the desired output if the dataframe with one more
+#'    column: the vector of weights. If FALSE returns only the vector. 
+#'    By default is TRUE. 
+#'
+#' @return
+#'  A dataframe or a numeric vector depending on return_df parameter.
+
+simple_weighting <- function(data, population,return_df=TRUE){
+  if( all(names(population) %in% names(data)) == FALSE ){
+    rlang::abort("Variable and population names don't match. Check list names.")}
+  df <- data[,names(population)]
+  
+  weights <- purrr::map(names(population), function(i){
+    if( all( names(population[[i]]) %in% unique(df[[i]]) ) == FALSE ){
+      rlang::abort("Variable and population target levels don't match. Check named vectors in the list.")}
+    
+    sample <- prop.table(table(df[[i]]))
+    population <- population[[i]][names(sample)]
+    weights <- population/sample
+    weights <- weights[ df[[i]] ]
+    return(weights)
+  } )
+  names(weights) <- names(population)
+  weights <- apply(dplyr::bind_cols(weights),1,prod)
+  if(return_df==FALSE) return(weights)
+  data$weights <- weights
+  return(data)
+}
+
+##### An example:
+
+### fake data
+set.seed(7)
+data <- tibble(
+  'gender' = as.factor(sample(c('F','M'),350,T,c(.3,.8))) ,
+  'age'= as.factor(sample(c('25','45','55'),350,T,c(.7,.2,.1))),
+  'vote'= sample(c('PP','PSOE','UP'),350,T,c(.4,.45,.15))
+  
+### population distributions of gender and age 
+population <- list( 'gender' = c('M'=.5,'F'=.5),
+                    'age' = c('25'=.50,'55'=.25,'45'=.25) )
+                    
+### new dataframe with the vector of weights based on gender and age
+data <- simple_weighting(data = data, population = population)
+  ## you can do as well:
+  #data$weights <- simple_weighting(data, population,FALSE)
+  #data <- data %>% mutate(weights=simple_weighting(data, population,FALSE))
+
+### check function behaviour
+check_simple_weighting <- function(df,var){
+  pro <- list()
+  pro$original <- round(prop.table( table(df[[var]]) )*100)
+  pro$ponderada <- round(prop.table( questionr::wtd.table(df[[var]],
+                                    weights = df[['weights']]))*100)
+  return(pro)
+}
+check_simple_weighting(data,'gender')
+check_simple_weighting(data,'age')
+check_simple_weighting(data_w,'vote')
